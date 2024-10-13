3fdcbef26b42c9411ae7e3ef61a1c4312da21d29
Ryzhenkov <Ryzhenkov>
2024-10-13 21:43:46 +0300

    Initial commit

---
+++ src/main/java/org/example/Interface.java
@@ -0,0 +1,8 @@
+package org.example;
+import java.util.List;
+
+public class Interface {
+  public static List<Integer> sort(List<Integer> list, VaritySorts algorithm) {
+    return algorithm.sort(list);
+  }
+}
---
+++ src/main/java/org/example/VaritySorts.java
@@ -0,0 +1,8 @@
+
+package org.example;
+
+import java.util.List;
+
+public interface VaritySorts {
+  List<Integer> sort(List<Integer> list);
+}
