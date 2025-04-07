eb5c07da6e6a60539341d50c88913178b088804e
Ulyalike <ryzhenkova.ue@phystech.edu>
2025-04-07 16:32:21 +0300

    Create UserUniversityRepository.java

---
+++ src/main/java/com/example/usermanagement/repository/UserUniversityRepository.java
@@ -0,0 +1,9 @@
+package com.example.usermanagement.repository;
+
+import com.example.usermanagement.model.UserUniversity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserUniversityRepository extends JpaRepository<UserUniversity, Long> {
+}
